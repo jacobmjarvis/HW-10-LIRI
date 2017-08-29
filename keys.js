8e1407bf85940f4e66c3eba0bef3918d52202dd0
Jacob Jarvis <jacobmjarvis@gmail.com>
2017-08-29 15:45:26 -0400

    Inital Commit
    Homework 10 - LIRI

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'Du14YCUxbGFfRacC0Cuuqo5PD',
+  consumer_secret: 'Mwx1nvJoaNuXKidClyQjoJOsiRtZ1Syn1PuRFUxFMEyi5tRDSB',
+  access_token_key: '21945466-corF8k7XDUHBplGFGOlBWctKKFJz1Qm7LUqC9CXOP',
+  access_token_secret: '5owFQhBBRGqpQJPvOjTs8WNDFLpmMkcOl2OKQ0MsUcEkK',
+}
